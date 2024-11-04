f3b68a1355a9eb55e34978bcd651751952d82d20
orazi-paolo <orazi.paolo10@gmail.com>
2024-11-04 17:31:36 +0100

    created page for all projects for authenticated users

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+@section('content')
+<div class="container mt-4">
+    <h1 class="mb-4">Admin Projects</h1>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Url</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($projects as $project)
+            <tr>
+                <td>{{ $project->id }}</td>
+                <td>{{ $project->name }}</td>
+                <td>{{ $project->description }}</td>
+                <td>{{ $project->url }}</td>
+                <td>
+                    <a href="#" class="btn btn-primary btn-sm">Show</a>
+                    <a href="#" class="btn btn-warning btn-sm">Edit</a>
+                    <a href="#" class="btn btn-danger btn-sm">Delete</a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
