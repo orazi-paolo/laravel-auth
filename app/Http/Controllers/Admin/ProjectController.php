143041b9aa5a550dfddd4d07d12018bfda71d9c6
orazi-paolo <orazi.paolo10@gmail.com>
2024-11-04 18:51:56 +0100

    created the destroy method

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -70,6 +70,8 @@ class ProjectController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $project = Project::findOrFail($id);
+        $project->delete();
+        return redirect()->route('admin.projects.index');
     }
 }
