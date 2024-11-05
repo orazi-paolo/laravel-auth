06b193c89b0b380cf3d68d67e11bba9ff02dbec2
orazi-paolo <orazi.paolo10@gmail.com>
2024-11-05 17:52:26 +0100

    split the routes for admins and guests in the header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
+             Paolo's projects
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -11,7 +11,13 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav me-auto">
-
+                <li>
+                    @auth
+                        <a class="nav-link" href="{{ route('admin.projects.index') }}">Projects</a>
+                    @else
+                        <a class="nav-link" href="{{ route('guest.projects.index') }}">Projects</a>
+                    @endauth
+                </li>
             </ul>
 
             <!-- Right Side Of Navbar -->
